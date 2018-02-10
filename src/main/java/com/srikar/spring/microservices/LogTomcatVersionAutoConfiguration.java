9a438265cd8667608765ee5c4ed1e343f3220621
Srikar Vedantam <srikar.vedantam@hpe.com>
2018-02-10 14:01:08 +0530

    ConditionalOnProperty illustration...

--- src/main/java/com/srikar/spring/microservices/LogTomcatVersionAutoConfiguration.java
+++ src/main/java/com/srikar/spring/microservices/LogTomcatVersionAutoConfiguration.java
@@ -3,6 +3,8 @@ package com.srikar.spring.microservices;
 import org.apache.catalina.startup.Tomcat;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Configuration;
 
 import javax.annotation.PostConstruct;
@@ -11,8 +13,8 @@ import javax.annotation.PostConstruct;
  * Created by vedantas on 2/10/2018.
  */
 @Configuration
-//@ConditionalOnClass(Tomcat.class)
-//@ConditionalOnProperty(name = "log-tomcat-version", matchIfMissing = true)
+@ConditionalOnClass(Tomcat.class)
+@ConditionalOnProperty(name = "log-tomcat-version", matchIfMissing = true)
 public class LogTomcatVersionAutoConfiguration {
     private static final Log logger = LogFactory.getLog(LogTomcatVersionAutoConfiguration.class);
 
